6a2e6addf14229fd8f8d819f430e14dc5bb07b81
akshit <akshit.singh20@gmail.com>
2021-04-27 15:33:10 +0530

    added course selection and redirection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask import redirect,request,abort
@@ -7,14 +7,23 @@ from flask_wtf.file import FileField
 from wtforms import SubmitField
 import os
 from werkzeug.utils import secure_filename
+from  courseFinder import *
 app = Flask(__name__)
 @app.route('/')
 def fun():
     return render_template('index.html')
 
+@app.route('/redirect',methods=['POST'])
+def algo():
+	lang=request.form['lang']
+	return redirect('/'+lang)
+
+
 @app.route('/<course>')
 def hello(course):
-    return course
+	# print(course)
+	return function(course)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
