4f05b9ea9001745c69fef737b8f4fa59d2016319
holychicken99 <akshit.singh20@gmail.com>
2021-05-03 15:19:06 +0530

    changed directory

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Course-Selector
-* [check website here](http://periwinkle-midnight-arithmetic.glitch.me)
--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, url_for,redirect
-from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
 from flask import redirect,request,abort
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
-from wtforms import SubmitField
+
 import os
-from werkzeug.utils import secure_filename
+
 from  courseFinder import *
 app = Flask(__name__)
 @app.route('/')
@@ -14,15 +12,25 @@ def fun():
     return render_template('index.html')
 
 @app.route('/redirect',methods=['POST'])
+
 def algo():
 	lang=request.form['lang']
 	return redirect('/'+lang)
 
+@app.route('/suggestion')
+def method_name():
+   return render_template('suggestionForm.html')
+
+
+
+
 
 @app.route('/<course>')
 def hello(course):
+
+
 	# print(course)
-	return function(course)
+	return render_template('result.html',array=function(course))
     
 
 if __name__ == '__main__':
